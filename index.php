36664d499baf3c9832b2855cdd14b258441bcb9d
alice <aliceusaoculos@gmail.com>
2022-08-12 13:55:22 -0300

    save

--- index.php
+++ index.php
@@ -1,18 +1,20 @@
+
 <!DOCTYPE html>
+<!--https://www.youtube.com/watch?v=bHRXRYTppHM-->
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Menu principal</title>
-    <link rel="stylesheet" href="estilo.css">
+    <title>Menu Principal</title>
+    <link rel="stylesheet" href="estilo_index.css">
+    
 </head>
 <body>
-    <h1>Menu principal</h1>
-    <div id="menu">
+    <h1>BEM VINDO AO DOIN</h1>
+    <div id="centro">
         <ul>
-            <li><a href="cadastro_produto.html"> Cadastrar Produto</a></li>
-            <li><a href="listar_usuarios.php"> Listar usuários</a></li>
+            <li><a href="cadastro_produto.html">Cadastre seu produto aqui!</li>
         </ul>
     </div>
 </body>
